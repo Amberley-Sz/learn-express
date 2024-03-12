6cf5483403d77e6e298708530c6e4ed35461a915
Zhan Su <su.zhan@northeastern.edu>
2024-03-12 18:29:42 -0400

    0311 activity

--- quiz/server.js
+++ quiz/server.js
@@ -29,12 +29,28 @@ app.use(
   cors({origin: 'http://localhost:3000'})
 );
 app.use('/read/usernames', addMsgToRequest);
+app.use('/read/username/:name', addMsgToRequest);
 
 app.get('/read/usernames', (req, res) => {
   let usernames = req.users.map(function(user) {
     return {id: user.id, username: user.username};
   });
   res.send(usernames);
+});
+app.get('/read/username/:name', (req, res) => {
+  let name = req.params.name;
+  let users_with_name = req.users.filter(function(user) {
+    return user.username === name;
+  });
+  console.log(users_with_name);
+  if (users_with_name.length === 0) {
+    res.send({
+      error: {message: `${name} not found`, status: 404}
+    });
+  }
+  else {
+    res.send(users_with_name);
+  }
 });
 
 app.use(express.json());
